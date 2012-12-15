22c2fbe4504b0c2c6b7124796d37742e43f10329
Ubuntu Web Developer <webdev@uwds.(none)>
2012-12-15 12:28:54 +0100

    Finish static pages

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -9,9 +9,9 @@ describe StaticPagesController do
     end
   end
 
-  describe "GET 'help --no-test-framework'" do
+  describe "GET 'help'" do
     it "returns http success" do
-      get 'help --no-test-framework'
+      get 'help'
       response.should be_success
     end
   end
